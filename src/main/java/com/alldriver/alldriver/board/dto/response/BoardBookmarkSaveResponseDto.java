1c0aa1cd5725373dddffdad2e4a94136ef72caab
seochae523 <seochae523@naver.com>
2024-08-23 16:17:39 +0900

    Feat : add getter to response dto

--- src/main/java/com/alldriver/alldriver/board/dto/response/BoardBookmarkSaveResponseDto.java
+++ src/main/java/com/alldriver/alldriver/board/dto/response/BoardBookmarkSaveResponseDto.java
@@ -2,9 +2,11 @@ package com.alldriver.alldriver.board.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
 @AllArgsConstructor
+@Getter
 public class BoardBookmarkSaveResponseDto {
     private Long boardId;
     private String userId;
